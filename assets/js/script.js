a9ec5a7b963b10ef38dc4ea409fe5050e885b911
dmharrison <dmharrison2014@gmail.com>
2024-04-10 19:09:37 -0400

    updated selector to id for add task button and modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
 console.log("you are linked");
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
-
+// let taskList = JSON.parse(localStorage.getItem("tasks"));
+// let nextId = JSON.parse(localStorage.getItem("nextId"));
+//query selectors for the correlating info to be stored
+const taskTitleInput = $("#task-title");
+const dueDateInput = $("#task-due-date");
+const taskDescripInput = $("#task-description");
+// const modal = $("#formModal");
+// const modalButton = $(".btn-success");
 // Todo: create a function to generate a unique task id
 function generateTaskId() {}
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {}
+function createTaskCard(task) {
+  // create a new div element to rep  the task card
+}
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {}
@@ -26,19 +33,35 @@ $(document).ready(function () {
   // Get references to the modal and the button that triggers it
   const modal = $(".modal");
   const modalButton = $(".btn-success");
+  console.log(modal);
+  console.log(modalButton);
 
   // Show the modal when the button is clicked
   modalButton.on("click", function () {
     modal.modal("show");
+    console.log("somthing should show up");
   });
 
   // Hide the modal when the close button is clicked
   $(".close").on("click", function () {
     modal.modal("hide");
+    console.log("click");
   });
 
   // Hide the modal when the "Add Task" button is clicked
   $(".btn-success").on("click", function () {
     modal.modal("hide");
+  });
+
+  $(".btn-success").on("click", function () {
+    let addTask = JSON.parse(localStorage.getItem("tasks")) || [];
+    const modalInputs = {
+      title: taskTitleInput.val(),
+      date: dueDateInput.val(),
+      description: taskDescripInput.val(),
+    };
+    addTask.push(modalInputs);
+    //save the updated tasks array back to local storage
+    localStorage.setItem("tasks", JSON.stringify());
   });
 }); //end of ready
